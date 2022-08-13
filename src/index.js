af86af71b230c3b3844a2b560f68feae3e21fb47
EmilHarutyunyan <emil.h098@gmail.com>
2022-08-13 11:24:46 +0000

    redux-structure

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import {FontStyle} from "./themes/FontStyle"
+import {GlobalStyle} from "./themes/GlobalStyle"
+// import setupInterceptors from "./services/setupInterceptors";
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  // <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <FontStyle />
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </Provider>
+  // </React.StrictMode>
+);
+
+// setupInterceptors(store);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
